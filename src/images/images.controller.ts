3fea738e9c8c308af5f2bcf2ed6a650c12e71648
Ximeo <ov3rskilled@gmail.com>
2025-04-30 15:23:15 +0300

    Get avatar validation fix

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Post, UseInterceptors, UploadedFile, Get, Param, Res } from '@nestjs/common'
+import {
+	Controller,
+	Post,
+	UseInterceptors,
+	UploadedFile,
+	Get,
+	Param,
+	Res,
+	ParseIntPipe,
+	BadRequestException,
+} from '@nestjs/common'
 import { FileInterceptor } from '@nestjs/platform-express'
 import { User } from '@prisma/client'
 import { CurrentUser } from 'src/auth/decorators/user.decorator'
@@ -9,28 +19,35 @@ import { Response } from 'express'
 
 @Controller('images')
 export class ImagesController {
-  constructor(private readonly imagesService: ImagesService) {}
+	constructor(private readonly imagesService: ImagesService) {}
 
-  @Post('avatar')
-  @UseInterceptors(FileInterceptor('avatar'))
-  @Auth()
-  async uploadAvatar(
-    @UploadedFile(ImageValidationPipe) file: Express.Multer.File,
-    @CurrentUser() user: User,
-    @Res() res: Response
-  ) {
-    await this.imagesService.processAndStoreAvatar(user.id, file.buffer)
-    const url = this.imagesService.getAvatarUrl(user.id, 128)
-    res.redirect(url)
-  }
+	@Post('avatar')
+	@UseInterceptors(FileInterceptor('avatar'))
+	@Auth()
+	async uploadAvatar(
+		@UploadedFile(ImageValidationPipe) file: Express.Multer.File,
+		@CurrentUser() user: User,
+		@Res() res: Response,
+	) {
+		await this.imagesService.processAndStoreAvatar(user.id, file.buffer)
+		const url = this.imagesService.getAvatarUrl(user.id, 128)
+		res.redirect(url)
+	}
 
-  @Get('avatar/:userId/:size')
-  async getAvatar(
-    @Param('userId') userId: string,
-    @Param('size') size: '64' | '128' | '512',
-    @Res() res: Response
-  ) {
-    const url = this.imagesService.getAvatarUrl(userId, Number(size))
-    res.redirect(url)
-  }
+	@Get('avatar/:userId/:size')
+	async getAvatar(
+		@Param('userId') userId: string,
+		@Param('size', ParseIntPipe) size: number,
+		@Res() res: Response,
+	) {
+		const allowedSizes = [64, 128, 512]
+		if (!allowedSizes.includes(size)) {
+			throw new BadRequestException(
+				'Неверный размер. Допустимые значения: 64, 128, 512',
+			)
+		}
+
+		const url = this.imagesService.getAvatarUrl(userId, size)
+		res.redirect(url)
+	}
 }
