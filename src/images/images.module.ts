d3d490e523f70c0971fde409ae01decf973e1159
Ximeo-dev <ov3rskilled@gmail.com>
2025-05-15 00:24:46 +0300

    Profile module exports fix

--- src/images/images.module.ts
+++ src/images/images.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common'
-import { UserModule } from 'src/user/user.module'
 import { ImagesController } from './images.controller'
 import { ImagesService } from './images.service'
 import { S3Module } from 'src/s3/s3.module'
